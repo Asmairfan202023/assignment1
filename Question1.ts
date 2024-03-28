44ae814340d27973f03e448b6705160b97f59b52
Asmairfan202023 <160836108+Asmairfan202023@users.noreply.github.com>
2024-03-28 13:47:35 +0500

    push this assignments to github

---
+++ Question1.ts
@@ -0,0 +1,18 @@
+// Question1: Write a program to find the greatest number of any three numbers by using if else statement.
+// Initializing three variables
+const no1 = 23;
+const no2 = 23;
+const no3 = 23;
+// Checking no1
+if(no1>no2 && no1>no3){
+    console.log("Number 1 is the greatest number");
+    // Checking no2
+} else if(no2>no1 && no2>no1){
+    console.log("Number Two is the greatest Number");
+    // checking no3
+}else if(no3>no1 && no3>no1){
+    console.log("Number Three is the greatest number");
+} else{
+// Checking if numbers are equal
+console.log("Numbers are Equal");
+}
---
+++ Question2.ts
@@ -0,0 +1,8 @@
+// Question2: Write a program to find the even and odd numbers by using if and else.
+// initializing variable
+let a=5;
+if(a%2 == 0){
+    console.log(`The given number: ${a} is even `);
+}else{
+    console.log(`The given number: ${a} is odd `);
+}
